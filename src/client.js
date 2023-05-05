f557f33fd1bfa5a4befd267a002688af205b42a9
Hyperscale <luca@weblutions.com>
2023-05-05 20:42:06 +1000

    Re-worked framework

--- src/client.js
+++ src/client.js
@@ -1,29 +1,52 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
+const config = require('../config.json')
 
-console.log("Electron.js App has started successfully...");
-
+console.log('Electron.js App has started successfully...')
 
 const createWindow = () => {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
+            preload: path.join(__dirname, '/events/preload.js'),
+            nodeIntegrationInWorker: true
         },
     })
-
-    mainWindow.loadFile('./src/pages/index.html')
+    mainWindow.loadURL(config.domain)
+    // mainWindow.loadFile('./src/pages/index.html')
 }
 
-app.whenReady().then(() => {
-    createWindow()
+const dockMenu = Menu.buildFromTemplate([
+    {
+        label: 'New Window',
+        click() {
+            console.log('New Window')
+        },
+    },
+    {
+        label: 'New Window with Settings',
+        submenu: [{ label: 'Basic' }, { label: 'Pro' }],
+    },
+    { label: 'New Command...' },
+])
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+app.whenReady()
+    .then(() => {
+        if (process.platform === 'darwin') {
+            app.dock.setMenu(dockMenu)
+        }
     })
-})
+    .then(createWindow)
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
